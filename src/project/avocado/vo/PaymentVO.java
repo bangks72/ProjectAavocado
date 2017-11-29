75e1761d3f3d520c2a9eb92613c8be8509ba89db
bluetoras <33773501+bluetoras@users.noreply.github.com>
2017-11-29 18:41:23 +0900

    VO 업데이트
    다들 받아라~

--- src/project/avocado/vo/PaymentVO.java
+++ src/project/avocado/vo/PaymentVO.java
@@ -2,43 +2,61 @@ package project.avocado.vo;
 
 public class PaymentVO {
 
-	private String id;
-	private String cardno;
-	private String mutong;
-
-	public PaymentVO(String id, String cardno, String mutong) {
-		super();
-		this.id = id;
-		this.cardno = cardno;
-		this.mutong = mutong;
-	}
+	private int mbno;// 멤버번호
+	private int payno;
+	private String paytype;
+	private String sdate;// 시작날짜 start-date
+	private int tkno;// 티켓종류 0,1,2,3 순으로 없음,1개월,3개월,12개월
 
 	public PaymentVO() {
-		// TODO Auto-generated constructor stub
 	}
 
-	public String getId() {
-		return id;
+	public PaymentVO(int mbno, int payno, String paytype, String sdate, int tkno) {
+		this.mbno = mbno;
+		this.payno = payno;
+		this.paytype = paytype;
+		this.sdate = sdate;
+		this.tkno = tkno;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public int getMbno() {
+		return mbno;
 	}
 
-	public String getCardno() {
-		return cardno;
+	public void setMbno(int mbno) {
+		this.mbno = mbno;
 	}
 
-	public void setCardno(String cardno) {
-		this.cardno = cardno;
+	public int getPayno() {
+		return payno;
 	}
 
-	public String getMutong() {
-		return mutong;
+	public void setPayno(int payno) {
+		this.payno = payno;
 	}
 
-	public void setMutong(String mutong) {
-		this.mutong = mutong;
+	public String getPaytype() {
+		return paytype;
+	}
+
+	public void setPaytype(String paytype) {
+		this.paytype = paytype;
+	}
+
+	public String getSdate() {
+		return sdate;
+	}
+
+	public void setSdate(String sdate) {
+		this.sdate = sdate;
+	}
+
+	public int getTkno() {
+		return tkno;
+	}
+
+	public void setTkno(int tkno) {
+		this.tkno = tkno;
 	}
 
 }
